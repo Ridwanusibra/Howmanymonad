6cfede03541f9906217a71f0031d6c506fb8f578
Ridwanusibra <167793564+Ridwanusibra@users.noreply.github.com>
2025-10-07 12:54:11 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        purplePrimary: '#5B21B6',
+      },
+    },
+  },
+  plugins: [],
+}
